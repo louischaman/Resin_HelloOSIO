dcbacc143dac72595aa069f7462d475c313b69d1
louischaman <louisschamrothgreen@gmail.com>
2015-03-23 12:37:55 +0000

    added python code for mqtt chat

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python
+import paho.mqtt.client as mqtt
+mqttc = mqtt.Client(client_id="1046")
+mqttc.username_pw_set("Louis", password="eMvuUIkq")
+
 import time
 counter = 0
 while counter<20:
 	time.sleep(3)
 	print "hello python!"
+	mqttc.connect("opensensors.io")
+	mqttc.publish("/users/Louis/test2", payload="Hello Opensensors!", qos=0, retain=False)
+	mqttc.disconnect();
+	
 	counter += 1
 	print counter
